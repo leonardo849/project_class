593514648f680458202ec90f1e460304d8e58ed3
leonardo849 <javascriptleonardolucas@gmail.com>
2025-09-10 18:55:33 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("That is the screen match");
+
+        int year = 2023;
+
+        boolean included = true;
+
+        double rating = 10;
+
+        double average  = (4.8 + 3.9 + 4.5)/3; 
+
+        System.out.printf("included: %b, rating: %f, average: %f, year: %d", included, rating, average, year);
+    }
+}
